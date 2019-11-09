46097916c02f54ab1c39468e2b4a2f1a9d8e5f8d
Emma3110 <54926198+Emma3110@users.noreply.github.com>
2019-11-09 10:00:40 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,74 +1,133 @@
-var o1=6;
-var o2=5;
-var o3=2;
-var i1=0;
-var i2=0;
-var i3=0;
-function pr1(){
-	if(document.getElementById('q1').value==o1){
-		document.getElementById('p1').innerHTML="true";
-		i1=1;
-	}
-	else
-	{
-		document.getElementById('p1').innerHTML="false";
-		i1=0;
-	}
-}
-
-function pr2(){
-	var check = document.getElementsByClassName('v2');
-	for (var i = 0;i < check.length; i++)
-	{
-		
-		if(check[i].checked==1)
-		{
-			if(check[i].value==o3)
-			{
-				document.getElementById('p2').innerHTML="true";
-				i2=1;
-			}
-			else
-			{
-				document.getElementById('p2').innerHTML="false";
-				i2=0;
-			}
-		}
-		
-	}
-
-}
-
-function pr3(var pr1, var pr2){
-	var check = document.getElementsByClassName('v3');
-	var test  = 0;
-	for (var i = 0; i < check.length; i++)
-	{
-		if(check[i].checked==1)
-		{
-			if((check[i].value==pr1)||(check[i].value==pr2)) test++; else test=o3+1;
-		}
-		if(test==o3)
-		{
-			document.getElementById('p3').innerHTML="true";
-			i3=1;
-		}
-		else
-		{
-			document.getElementById('p3').innerHTML="false";
-			i3=0;
-		}
-	}
-}
-
-function end(){
-	
-	for(var i=0;i<8;i++)
+function q111()
 {
-	pr1(i);
-}
-	pr2();
-	pr3();
+var otvet=[2,6,4,3,3,17100000,4,2,4,1];	
+var ball=0;	
+var mass=document.getElementsByClassName('v2');
+
+for (var i = 0;i < mass.length; i++)
+	{
+		if(mass[i].checked==1)
+		{
+			if(mass[i].value==otvet[0])	ball++;
+		}
+	}
+
+mass=document.getElementsByClassName('v1');
+for (var i = 0;i < mass.length; i++)
+	{
+		if(mass[i].checked==1)
+		{
+			if(mass[i].value==otvet[1])	ball++;
+		}
+	}
+
+mass=document.getElementsByClassName('v3');
+for (var i = 0;i < mass.length; i++)
+	{
+		if(mass[i].checked==1)
+		{
+			if(mass[i].value==otvet[2])	ball++;
+		}
+	}
+
+mass=document.getElementsByClassName('v6');
+for (var i = 0;i < mass.length; i++)
+	{
+		if(mass[i].checked==1)
+		{
+			if(mass[i].value==otvet[6])	ball++;
+		}
+	}
+
+mass=document.getElementsByClassName('v8');
+for (var i = 0;i < mass.length; i++)
+	{
+		if(mass[i].checked==1)
+		{
+			if(mass[i].value==otvet[8])	ball++;
+		}
+	}
+
+mass=document.getElementsByClassName('v9');
+for (var i = 0;i < mass.length; i++)
+	{
+		if(mass[i].checked==1)
+		{
+			if(mass[i].value==otvet[9])	ball++;
+		}
+	}
 	
-	document.getElementById('end').innerHTML=ball;
+
+
+mass = document.getElementsByClassName('v4');
+	var test  = 0;
+	for (var i = 0; i < mass.length; i++)
+	{
+		if(mass[i].checked==1)
+		{
+			if(mass[i].value==1) test++; else test=4;
+		}
+	}	
+	if (test==otvet[3])
+		{
+		ball++;
+		}
+	
+
+mass = document.getElementsByClassName('v5');
+	var test  = 0;
+	for (var i = 0; i < mass.length; i++)
+	{
+		if(mass[i].checked==1)
+		{
+			if(mass[i].value==1) test++; else test=4;
+		}
+	}	
+	if (test==otvet[4])
+		{
+		ball++;
+		}
+	
+
+mass = document.getElementsByClassName('v7');
+	var test  = 0;
+	for (var i = 0; i < mass.length; i++)
+	{
+		if(mass[i].checked==1)
+		{
+			if(mass[i].value==1) test++; else test=4;
+		}
+	}	
+	if (test==otvet[7])
+		{
+		ball++;
+		}
+	
+if(document.getElementById('q1').value==otvet[5]) ball++;
+
+
+
+
+
+if (ball==10)
+{
+	document.getElementById('end2').innerHTML="Молодец! Все задания верные!Количество верных ответов:"+ball;
+	
 }
+
+
+if ((ball<10)&&(ball>5))
+{
+	document.getElementById('end2').innerHTML="Хорошо, но могло быть и лучше! Нужно больше читать про Россия. Количество верных ответов:"+ball;
+}
+if ((ball<5)&&(ball>=0))
+{
+	document.getElementById('end2').innerHTML="Вы плохо знаете Россию. Количество верных ответов:"+ball;
+}
+
+
+
+
+}
+
+
